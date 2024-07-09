7fd7c9fe341c333c315dd60dc2e1aeb07a407ad3
Captain-Kim <brighthero@kakao.com>
2024-07-09 17:33:36 +0900

    refactor: 갤리러형 보기 컴포넌트 UI 개선 (ref.와이어프레임)

--- src/components/MyPage/PostVIew/MyPostGalleyView.tsx
+++ src/components/MyPage/PostVIew/MyPostGalleyView.tsx
@@ -2,12 +2,17 @@ const MyPostGalleryView = ({ posts }: { posts: any[] }) => {
     return (
         <div className="grid grid-cols-3 gap-4">
             {posts.map((post) => (
-                <div key={post.id} className="p-4 bg-white shadow rounded">
-                    <img src="https://img.sbs.co.kr/newsnet/etv/upload/2023/04/06/30000838382.jpg" alt={post.title} className="w-full h-40 object-cover rounded mb-2" />
-                    <h3 className="text-lg font-medium">{post.title}</h3>
-                    <p className="mt-2 text-gray-600">{post.content}</p>
+                <div key={post.id} className="p-4">
+                    <img src="https://img.sbs.co.kr/newsnet/etv/upload/2023/04/06/30000838382.jpg" alt={post.title} className="w-full h-48 object-cover rounded-2xl mb-2" />
+                    <div className="flex items-center justify-between">
+                        <span className="text-lg font-medium">{post.title}</span>
+                        <div className="flex items-center space-x-2">
+                            <span className="text-main-green">❤</span>
+                            <span>10</span>
+                        </div>
+                    </div>
+                    <p className="mt-2 text-gray-600 h-20">{post.content}</p>
                     <div className="flex items-center space-x-2 mt-2 text-gray-500 text-sm">
-                        <span>❤ 10</span>
                         <span>{new Date(post.created_at).toLocaleDateString()}</span>
                     </div>
                 </div>
