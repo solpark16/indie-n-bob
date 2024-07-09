4d4f28a020fd2565476d10d2babee059667c79b2
Captain-Kim <brighthero@kakao.com>
2024-07-09 17:10:26 +0900

    style: 내 글 리스트 보기 방식 변경 버튼 일반 버튼에서 와이어프레임 아이콘으로 변경

--- src/components/MyPage/PostVIew/MyPostViewSwitcher.tsx
+++ src/components/MyPage/PostVIew/MyPostViewSwitcher.tsx
@@ -10,12 +10,13 @@ export default function PostSwitcher({ posts }: { posts: any[] }) {
 
     return (
         <>
+        {/* 클릭 시 아이콘 색상 바꾸는 것 꽤 복잡하여 일단 배경 음영 바뀌는 것으로 구현 */}
             <div className="flex items-center justify-end space-x-4 mt-4">
-                <button className={`px-4 py-2 ${viewType === 'list' ? 'bg-gray-200' : 'bg-white'} border rounded`} onClick={() => setViewType('list')}>
-                    리스트형
+                <button className={`px-4 py-2 ${viewType === 'list' ? 'bg-gray-200' : 'bg-white'}`} onClick={() => setViewType('list')}>
+                    <img src="/List_View_Icon.svg" alt="List View Icon" className="w-4 h-4" />
                 </button>
-                <button className={`px-4 py-2 ${viewType === 'gallery' ? 'bg-gray-200' : 'bg-white'} border rounded`} onClick={() => setViewType('gallery')}>
-                    갤러리형
+                <button className={`px-4 py-2 ${viewType === 'gallery' ? 'bg-gray-200' : 'bg-white'}`} onClick={() => setViewType('gallery')}>
+                    <img src="/Gallery_View_Icon.svg" alt="Gallery View Icon" className="w-4 h-4" />
                 </button>
             </div>
             <div className="mt-4">
