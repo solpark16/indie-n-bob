5a6e32eac374ab475aba79c2b4f92f646fda15ab
reeny404 <happy2hyeon@kakao.com>
2024-07-09 17:34:35 +0900

    chore : remove files

--- src/app/(provider)/(root)/posts/[postId]/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-function Postlayout({ children }: PropsWithChildren) {
-  return <div className="max-w-[700px] mx-auto">{children}</div>;
-}
-
-export default Postlayout;
