da1ccd4793e7cb243ccc6ffdb1eee2c3cb483f4f
oneieo <sun020716@gmail.com>
2024-07-09 19:30:06 +0900

    chore : comment 등록 api 함수 추가

--- src/components/CommentList/Comments.tsx
+++ src/components/CommentList/Comments.tsx
@@ -6,7 +6,7 @@ import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 const Comments = ({ postId }: Params) => {
   return (
     <div className="w-full flex flex-col justify-center items-center border-t border-[#dddddd]">
-      <CommentUpload />
+      <CommentUpload postId={postId} />
       <CommentsView postId={postId} />
     </div>
   );
