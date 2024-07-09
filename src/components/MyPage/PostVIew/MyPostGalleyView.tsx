e0a729426fed989f47b9e16f9e37797ef3ae969a
Captain-Kim <brighthero@kakao.com>
2024-07-09 22:40:22 +0900

    feat: add gallery형 보기 컴포넌트 로그인 한 유저의 정보를 posts 테이블에서 찾아오도록 기능 구현

--- src/components/MyPage/PostVIew/MyPostGalleyView.tsx
+++ src/components/MyPage/PostVIew/MyPostGalleyView.tsx
@@ -1,17 +1,17 @@
 const MyPostGalleryView = ({ posts }: { posts: any[] }) => {
     return (
         <div className="grid grid-cols-3 gap-4">
-            {posts.map((post, index) => (
-                <div key={index} className="p-4 ease-in-out duration-400 transition-transform transform hover:-translate-y-2">
-                    <img src="https://img.sbs.co.kr/newsnet/etv/upload/2023/04/06/30000838382.jpg" alt={post.title} className="w-full h-48 object-cover rounded-2xl mb-2" />
+            {posts.map((post) => (
+                <div key={post.post_id} className="p-4 ease-in-out duration-400 transition-transform transform hover:-translate-y-2">
+                    <img src={post.image} alt={post.title} className="w-full h-48 object-cover rounded-2xl mb-2" />
                     <div className="flex items-center justify-between">
-                        <span className="text-lg font-medium">{post.title}</span>
+                        <span className="text-lg font-medium truncate">{post.title}</span>
                         <div className="flex items-center space-x-2">
                             <span className="text-primary">❤</span>
                             <span>10</span>
                         </div>
                     </div>
-                    <p className="mt-2 text-gray-600 h-20">{post.content}</p>
+                    <p className="mt-2 text-gray-600 line-clamp-3">{post.content}</p>
                     <div className="flex items-center space-x-2 mt-2 text-gray-500 text-sm">
                         <span>{new Date(post.created_at).toLocaleDateString()}</span>
                     </div>
