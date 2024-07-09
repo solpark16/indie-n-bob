0bf683cdb191ec7e94b19394f9d7ab0a6a3907fd
Captain-Kim <brighthero@kakao.com>
2024-07-09 16:45:52 +0900

    style: mypage 선호하는 뮤지션 해시태그 수평 정렬 및 태그 간격 gap-2로 조정

--- src/app/(provider)/mypage/page.tsx
+++ src/app/(provider)/mypage/page.tsx
@@ -30,11 +30,11 @@ export default function MyPage() {
                                     <ProfileEditButton />
                                 </div>
                                 <div className='mt-2'>
-                                    <p>
+                                    <p className="flex items-center">
                                         <img src="/favorite_artist_icon.svg" alt="Favorite Artist Icon" className="inline-block w-4 h-4 mr-2 animate-pulse" />
                                         선호하는 뮤지션
                                     </p>
-                                    <div className="flex flex-wrap space-x-2 mt-4">
+                                    <div className="flex flex-wrap gap-2 mt-4">
                                         {tags.map((tag, index) => (
                                             <span key={index} className="text-sm text-main-green">#{tag}</span>
                                         ))}
