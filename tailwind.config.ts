434b234ac0d828ff985a6a17337b412355880c47
Captain-Kim <brighthero@kakao.com>
2024-07-09 16:38:50 +0900

    fix: TailwindCSS main-green 컬러 헥스코드 오타 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'main-green': '#10af85',
+        'main-green': '#10af86',
       },
     },
   },
