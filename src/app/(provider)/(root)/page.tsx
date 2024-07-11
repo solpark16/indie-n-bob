437a9e427213e744b5de591bf81e840e6c9dff77
t2023-m0081 <t2023-m0081@t2023-m0081ui-MacBookAir.local>
2024-07-11 15:17:51 +0900

    alert 필요할까?

--- src/app/(provider)/(root)/page.tsx
+++ src/app/(provider)/(root)/page.tsx
@@ -1,19 +1,10 @@
 "use client";
-import { AlertUi } from "@/components/Alert";
-import Carousel from "@/components/Mainpage/Carousel";
+
 import MainPage from "@/components/Mainpage/MainPage";
-import { useAlertStore } from "@/zustand/alert.store";
 
 export default function HomePage() {
-  const { setAlert } = useAlertStore();
-  const buttonClick = () => {
-    setAlert(true, `반갑습니다!👋`, "테스트");
-  };
   return (
     <div>
-      <button onClick={buttonClick}>클릭</button>
-      <AlertUi />
-
       <MainPage />
     </div>
   );
