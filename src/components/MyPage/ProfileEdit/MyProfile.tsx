2ad3fb94236b25a43303f2bfa98332d55cde6b8f
Captain-Kim <brighthero@kakao.com>
2024-07-12 21:36:48 +0900

    refactor: MyProfile 컴포넌트 img 태그 Image 컴포넌트로 최적화

--- src/components/MyPage/ProfileEdit/MyProfile.tsx
+++ src/components/MyPage/ProfileEdit/MyProfile.tsx
@@ -35,7 +35,12 @@ export default function MyProfile() {
                     <hr className="border-gray-300 w-full mt-4" />
                     <div className='mt-2'>
                         <p className="flex items-center">
-                            <img src="/favorite_artist_icon.svg" alt="Favorite Artist Icon" className="inline-block w-4 h-4 mr-2 animate-pulse" />
+                            <Image src="/favorite_artist_icon.svg"
+                                alt="Favorite Artist Icon"
+                                width={20}
+                                height={20}
+                                className="inline-block w-4 h-4 mr-2 animate-pulse"
+                            />
                             선호하는 뮤지션
                         </p>
                         <div className="flex flex-wrap gap-2 mt-2">
