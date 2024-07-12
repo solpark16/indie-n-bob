e7cde95f7b8460481c78862a90d1ada03b646e09
reeny404 <happy2hyeon@kakao.com>
2024-07-12 18:18:51 +0900

    feat : 게시판 무한 스크롤 추가

---
+++ src/hooks/useInfinitePosts.tsx
@@ -0,0 +1,43 @@
+import SITE_URL from "@/constant";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useMemo } from "react";
+
+function useInfinitePosts() {
+  const {
+    data,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["posts"],
+    queryFn: async ({ pageParam = 0 }) => {
+      const {
+        data: { posts, hasNext, currentPageNo },
+      } = await axios.get(`${SITE_URL}/api/posts?pageNo=${pageParam}`);
+
+      return { posts, nextCursor: hasNext ? currentPageNo + 1 : null };
+    },
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+    initialPageParam: 0,
+  });
+
+  const posts = useMemo(() => {
+    console.log("parse Posts !! ");
+    if (!data?.pages) return;
+    return data.pages.flatMap((page) => page.posts);
+  }, [data]);
+
+  return {
+    posts,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  };
+}
+
+export default useInfinitePosts;
