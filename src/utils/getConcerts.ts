928d14c1b8e737c282e76c83116b3365ee93ad34
Sol Park <solpark16@gmail.com>
2024-07-13 20:02:15 +0900

    fix : 정렬 시 무한 스크롤 중단 오류 해결

--- src/utils/getConcerts.ts
+++ src/utils/getConcerts.ts
@@ -1,18 +1,34 @@
 import supabase from "./supabase/client";
 
-export default async function getMyPosts(page = 0, limit = 6) {
+export default async function getConcerts(page = 0, sort = "latest") {
+    const limit = 6
 
+    let query = supabase.from('concert_posts').select(`*, concert_likes(post_id)`, { count: 'exact' });
+    
+      if (sort === "latest"){
+        query = query.order('created_at', { ascending: false })
+      } else if (sort === "imminent") {
+        query = query.order('end_date', {ascending:true})
+      }
 
-    const { data: posts, error: postError, count } = await supabase
-        .from('concert_posts')
-        .select(`*, concert_likes(post_id)`, { count: 'exact' })
-        .order('created_at', { ascending: false })
-        .range(page * limit, (page + 1) * limit - 1);
+      const {data:posts, error: postError, count} = await query
 
-        if (postError) {
-          throw new Error(postError.message);
-        }
-    const nextCursor = (page + 1) * limit < count ? page + 1 : null;
+      if (postError) {
+        throw new Error(postError.message);
+      }
 
-    return { posts, nextCursor };
+      let sortedPosts = posts;
+      if (sort === "ranking"){
+        sortedPosts = posts.sort((a,b)=>b.concert_likes.length - a.concert_likes.length)
+      }
+
+      const start = page * limit;
+      const end = start + limit;
+      const paginatedPosts = sortedPosts.slice(start, end);
+
+      if (count){
+        const nextCursor = end < count ? page + 1 : null;
+        return { posts: paginatedPosts, nextCursor };
+      }
+    return { posts: paginatedPosts, nextCursor: null };
 }
